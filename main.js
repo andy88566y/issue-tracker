5a8b674437b7c04e594665c7b5ae546bf8bb764f
GCD <andy88566y@gmail.com>
2019-04-29 15:52:30 +0800

    implement saveIssue()

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
 function fetchIssues() {
   var issues = JSON.parse(localStorage.getItem('issues'));
@@ -25,3 +25,33 @@ function fetchIssues() {
   }
 }
 
+function saveIssue(e) {
+  var issueId = chance.guid(); // what?
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueStatus = 'Open';
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+  
+  if (localStorage.getItem('issues') === null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+  
+  document.getElementById('issueInputForm').reset();
+ 
+  fetchIssues();
+  
+  e.preventDefault(); 
+}
